cecb22bd4d210645e8837d9e78b8af6c4399ec8f
nayonglin <nayonglin@outlook.com>
2018-05-25 17:30:43 +0800

    无new构建

---
+++ linQuery.js
@@ -0,0 +1,32 @@
+
+(function (window){
+
+    // 定义本地linQuery
+    linQuery = function (name) {
+        return new linQuery.prototype.init(name);
+    };
+
+    
+
+   
+
+    linQuery.fn = linQuery.prototype = {
+        constructor: linQuery,
+        init: function (name) {
+            this.name = name;
+            return this;
+        },
+        logName: function() {
+            console.log(this.name);
+        },
+    }
+
+
+   linQuery.prototype.init.prototype = linQuery.prototype;  // 
+   window.$ = window.linQuery = linQuery;  // 在window下定义$
+})(window);
+
+
+var a = $("xixi");
+a.logName();
+$.Callbacks();
